445e9f64ef6d766ecdb1e5f25f0378832556f8bc
margaret lamotte <topmuch@gmail.com>
2026-01-02 18:38:01 +0000

    Update main page to Dashboard

--- src/pages.config.js
+++ src/pages.config.js
@@ -1,39 +1,39 @@
 import AddProduct from './pages/AddProduct';
+import AdminBanners from './pages/AdminBanners';
+import AdminCampaigns from './pages/AdminCampaigns';
 import AdminPanel from './pages/AdminPanel';
 import Dashboard from './pages/Dashboard';
 import Demo from './pages/Demo';
 import Home from './pages/Home';
 import ProductPage from './pages/ProductPage';
 import ProfileSetup from './pages/ProfileSetup';
+import SellerCampaigns from './pages/SellerCampaigns';
 import Shop from './pages/Shop';
 import ShopPage from './pages/ShopPage';
-import TikTokGuide from './pages/TikTokGuide';
-import AdminBanners from './pages/AdminBanners';
-import AdminCampaigns from './pages/AdminCampaigns';
-import SellerCampaigns from './pages/SellerCampaigns';
-import SubscriptionRequest from './pages/SubscriptionRequest';
 import SubscriptionExpired from './pages/SubscriptionExpired';
+import SubscriptionRequest from './pages/SubscriptionRequest';
+import TikTokGuide from './pages/TikTokGuide';
 
 
 export const PAGES = {
     "AddProduct": AddProduct,
+    "AdminBanners": AdminBanners,
+    "AdminCampaigns": AdminCampaigns,
     "AdminPanel": AdminPanel,
     "Dashboard": Dashboard,
     "Demo": Demo,
     "Home": Home,
     "ProductPage": ProductPage,
     "ProfileSetup": ProfileSetup,
+    "SellerCampaigns": SellerCampaigns,
     "Shop": Shop,
     "ShopPage": ShopPage,
-    "TikTokGuide": TikTokGuide,
-    "AdminBanners": AdminBanners,
-    "AdminCampaigns": AdminCampaigns,
-    "SellerCampaigns": SellerCampaigns,
-    "SubscriptionRequest": SubscriptionRequest,
     "SubscriptionExpired": SubscriptionExpired,
+    "SubscriptionRequest": SubscriptionRequest,
+    "TikTokGuide": TikTokGuide,
 }
 
 export const pagesConfig = {
-    mainPage: "Home",
+    mainPage: "Dashboard",
     Pages: PAGES,
 };
